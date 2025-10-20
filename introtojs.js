5cdc7872cb62f72b1db347f9f0711acde0e3d70d
Ibrahima Bah <bibrahim97@outlook.com>
2025-10-21 00:05:11 +0100

    intro to js

---
+++ introtojs.js
@@ -0,0 +1,24 @@
+//An Arithmatic operator
+    console.log(26 + 3.5)   // prints 29.5 (addition operator)
+    console.log(2025 - 1969)   // prints 56 (subtraction operator)
+    console.log(65 / 240)   // prints 0.2708333333333333 (division operator)
+    console.log(0.2708 * 100)   // prints 27.08 (multiplication operator)
+    console.log(11 % 3); // prints 2 (remainder operator)
+    console.log(12 % 3); // Prints 0 (remainder operator)
+
+//String Concatenation
+    console.log('Hello' + 'World') //prints HelloWorld
+    console.log('Hello ' + 'World') //prints Hello World
+    
+//Properties 
+    console.log('Teaching the world how to code'.length) //prints 30
+
+//Methods
+    console.log('Codecademy'.toUpperCase()); // prints 'CODECADEMY'
+    console.log('    Remove whitespace   '.trim()); // prints 'Remove whitespace'
+
+//Built-in Objects
+console.log(Math.random() *100) //prints a random number between 0 and 100
+console.log(Math.floor(Math.random()*100)) //prints a random integer between 0 and 99
+console.log(Math.ceil(43.8)) //prints 44
+console.log(Number.isInteger(2017)) //prints true
