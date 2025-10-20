2f4cdf3b3546904889b61494a42278ed268488b3
Ibrahima Bah <bibrahim97@outlook.com>
2025-10-21 00:43:39 +0100

    variables

---
+++ variables.js
@@ -0,0 +1,58 @@
+//Var variable
+var favoriteFood = 'pizza'; // String variable
+var numOfSlices = 8; // Number variable
+console.log(favoriteFood);  // prints pizza
+console.log(numOfSlices); // prints 8
+
+//Let variable
+let changeMe = true; // Boolean variable
+changeMe = false; // Reassigning the value of changeMe
+console.log(changeMe); // prints false
+
+//Const variable
+const entree = 'Enchiladas'; // String constant
+console.log(entree) // prints Enchiladas
+
+//math operators with let
+let levelUp = 10; 
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+levelUp += 5 // adds 5 to levelUp
+powerLevel -= 100 // subtracts 100 from powerLevel
+multiplyMe *= 11 // multiplies multiplyMe by 11
+quarterMe /= 4 // divides quarterMe by 4
+
+console.log('The value of levelUp:', levelUp); // prints 15
+console.log('The value of powerLevel:', powerLevel); // prints 8901
+console.log('The value of multiplyMe:', multiplyMe); // prints 352
+console.log('The value of quarterMe:', quarterMe);  // prints 288
+
+
+//increment and decrement with let
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar++; // increments gainedDollar by 1
+lostDollar--; // decrements lostDollar by 1
+
+//concatenate strings with let
+let favoriteAnimal = 'Cat'
+console.log('My favorite animal: ' + favoriteAnimal) // prints My favorite animal: Cat
+
+//string interpolation with let
+let myName = 'Ibrahim ibn Muhammad Alameen Telemele'
+let myCity = 'Madina Almunawarah'
+console.log(`My name is ${myName}. My favorite city is ${myCity}`) // prints My name is Ibrahim ibn Muhammad Alameen Telemele. My favorite city is Madina Almunawarah
+
+//typeof operator with let
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable) // prints string
+newVariable = 1; // reassigning newVariable to a number
+console.log(typeof newVariable) // prints number
+
+
+
+
+
