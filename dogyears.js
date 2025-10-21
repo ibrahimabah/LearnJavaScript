c71b2fc8c3803f2a1356978998f815dc2242f499
Ibrahima Bah <bibrahim97@outlook.com>
2025-10-22 00:36:44 +0100

    dogyears: update

--- dogyears.js
+++ dogyears.js
@@ -1,12 +1,12 @@
-let myAge = 26; // your age in years
+let myAge = 26; // This variable stores my age in human years
 let earlyYears = 2; // first two years of a dog's life
 earlyYears *= 10.5; // converting early years to dog years
 
-let laterYears = myAge - 2; // remaining years after the first two
+let laterYears = myAge - 2; // Subtract 2 from myAge to get the number of later years
 laterYears *= 4; // converting later years to dog years
 console.log(earlyYears); // prints 21
 
 let myAgeInDogYears = earlyYears + laterYears; // total age in dog years
 let myName = 'Ibrahim ibn Muhammad Alameen Telemele'.toLowerCase(); // your name in lowercase
 
-console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years`); // printing the final statement using string interpolation
+console.log(`My name is ${myName}. I am ${myAge} in human years, which ${myAgeInDogYears} years old in dog years`); // printing the final statement using string interpolation
