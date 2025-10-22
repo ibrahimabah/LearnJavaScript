9a694d4297e8fc7aa86c85fe154f533e8edcf601
Ibrahima Bah <bibrahim97@outlook.com>
2025-10-23 00:07:31 +0100

    conditional statements

---
+++ conditionals/statements.js
@@ -0,0 +1,115 @@
+// if statements
+let sale = true; 
+sale = false;
+if (sale){
+  console.log('Time to buy!')
+}
+
+// if...else statements
+else {
+  console.log('Time to wait for a sale.')
+}
+
+// comparison operators
+let hungerLevel = 7;
+if ( hungerLevel > 7) {
+  console.log('Time to eat!')
+} else {
+  console.log('We can eat later!')
+}
+
+// logical operators
+let mood = 'sleepy';
+let tirednessLevel = 6;
+
+if ( mood === 'sleepy' && tirednessLevel > 8) {
+  console.log('time to sleep')
+} else {
+  console.log('not bedtime yet')
+}
+
+// truthy and falsy values
+let wordCount = 1;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
+// truthy and falsy assignments
+let tool = '';
+
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+tool = 'marker';
+
+// ternary operator
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.')
+: console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!')
+: console.log('Incorrect!');
+
+favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ?
+console.log('I love that!') :
+console.log("I don't love that!");
+
+// else if statements
+let season = 'summer';
+
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+} else if ( season === 'winter') {
+  console.log('It\'s winter! Everything is covered in snow.')
+} else if ( season === 'fall') {
+  console.log('It\'s fall! Leaves are falling!')
+} else if ( season === 'summer') {
+  console.log('It\'s sunny and warm because it\'s summer!')
+} else {
+  console.log('Invalid season.');
+}
+
+// switch statements
+let athleteFinalPosition = 'first place';
+
+switch (athleteFinalPosition) {
+  case 'first place':
+    console.log('You get the gold medal!');
+    break;
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+  case 'third place':
+    console.log('You get the bronze medal!');
+    break; 
+  default:
+    console.log('No medal awarded.');
+    break;  
+}
+
+// reviw practice
+let myAge = 26;
+
+if (myAge > 28) {
+  console.log('Your father is Thierno on paper');
+} else {
+  console.log('Your father is Muhammad Al`ameen, May Allah have mercy on him')
+}
+
