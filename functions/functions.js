fea19ce2688d583dfd35170e1d104a9d83e2b04f
Ibrahima Bah <bibrahim97@outlook.com>
2026-02-22 00:28:55 +0000

    helper functions

--- functions/functions.js
+++ functions/functions.js
@@ -1,3 +1,4 @@
+/*
 // functions declaration
 function getReminder(){
   console.log('Water the plants.');
@@ -14,3 +15,35 @@ function sayThanks() {
 
 sayThanks() // prints the message in  the function
 
+// parameters and arguments
+function sayThanks(name) { // the parameter 'name' is declared here
+  console.log('Thank you for your purchase '+ name +'! We appreciate your business.'); // 'name' parameter can be used as a variable in the function
+}
+
+sayThanks('Cole') // 'Cole' is the argument passed to the function
+
+// default parameters
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs') { // default parameters are set here
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+*/
+
+// return 
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+const numOfMonitors = monitorCount(5, 4);
+
+console.log(numOfMonitors);
+
+
+// helper functions
+function costOfMonitors(rows, columns) {
+  return monitorCount(rows, columns) * 200
+};
+
+const totalCost =  costOfMonitors(5,4);
+console.log(totalCost)
